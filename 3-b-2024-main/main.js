bcff548bd3b4e1f8f3dbcfc91c23522630e30e69
larissadiaschejowich <129761676+larissadiaschejowich@users.noreply.github.com>
2024-05-02 14:00:15 -0300

    Add files via upload

--- 3-b-2024-main/main.js
+++ 3-b-2024-main/main.js
@@ -15,5 +15,12 @@ for (let i=0; i < botoes.length; i++) {
 }
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
+contadores[0].textContent = calculatempo(tempoObjetivo1)
 let tempoAtual = new Date();
 contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+
+
+function calculatempo(tempoObjetivo) {
+    let tempoAtual = new Date();
+    let tempoFinal
+}
